f2fba9dec5f7f148c182caa3837a3cdb1a0a9d6f
jfok1972 <jfok1972@qq.com>
2021-02-12 10:07:47 +0800

    init

--- src/pages/module/additionalAction/systemAction.tsx
+++ src/pages/module/additionalAction/systemAction.tsx
@@ -1,258 +1,319 @@
 import React from 'react';
 import { message, Modal } from 'antd';
 import { Dispatch } from 'redux';
-import { ModuleModal, ModuleState, AdditionFunctionModal } from '../data';
-import { queryCreatePersonnelUser, queryResetUserPassword } from './systemActionService';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { DisplayUserLimits, SetUserLimits, SetRoleLimits } from './userLimit';
 import { DrawerProps } from 'antd/lib/drawer';
 import request from '@/utils/request';
-import { activitiModeler } from '../approve/ProcessManage/activitiModeler';
 import { apply, download } from '@/utils/utils';
-import { businessActions } from './businessAction';
 import { setGlobalDrawerProps } from '@/layouts/BasicLayout';
+import { ModuleModal, ModuleState, AdditionFunctionModal } from '../data';
+import { queryCreatePersonnelUser, queryResetUserPassword } from './systemActionService';
+import { DisplayUserLimits, SetUserLimits, SetRoleLimits } from './userLimit';
+import { activitiModeler } from '../approve/ProcessManage/activitiModeler';
+import { businessActions } from './businessAction';
 import { importTableAndView, refreshFields } from './importTableAndView';
 import { breakDataSource, testDataSource, importSchema } from './dataSource';
 import { dataSourceImportTableAndView } from './dataSourceImportTableAndView';
 
 export interface ActionParamsModal {
-    moduleInfo: ModuleModal,
-    moduleState: ModuleState | any,
-    funcDefine: AdditionFunctionModal,
-    dispatch: Dispatch,
-    record?: any,
-    records?: any[],
+  moduleInfo: ModuleModal;
+  moduleState: ModuleState | any;
+  funcDefine: AdditionFunctionModal;
+  dispatch: Dispatch;
+  record?: any;
+  records?: any[];
 }
 
 interface RefreshRecordParams {
-    dispatch: Dispatch,
-    moduleName: string,
-    key: string,
+  dispatch: Dispatch;
+  moduleName: string;
+  key: string;
 }
 
 interface UserDrawerProps extends DrawerProps {
-    children: any,
+  children: any;
 }
 
 /**
  * 根据模块名称和主键更新数据，更新好处会更新到moduleState中
- * @param params 
+ * @param params
  */
 const refreshRecordByKey = (params: RefreshRecordParams) => {
-    const { dispatch, moduleName, key } = params;
-    dispatch({
-        type: 'modules/refreshRecord',
-        payload: {
-            moduleName,
-            recordId: key,
-        },
-    })
-}
+  const { dispatch, moduleName, key } = params;
+  dispatch({
+    type: 'modules/refreshRecord',
+    payload: {
+      moduleName,
+      recordId: key,
+    },
+  });
+};
 
 /**
  * 重置一个用户的密码为123456
- * @param params 
+ * @param params
  */
 const resetUserPassword = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield } } = params;
-    const username = record[namefield];
-    Modal.confirm({
-        title: `确定要重置用户『${username}』的密码吗?`,
-        icon: <ExclamationCircleOutlined />,
-        onOk() {
-            queryResetUserPassword({ userid: record[primarykey] })
-                .then((response: any) => {
-                    if (response.success)
-                        message.success(`用户『${username}』的密码已重置为“123456”请通知其尽快修改!`);
-                    else
-                        message.error(`用户『${username}』的密码已重置失败!`);
-                })
-        }
-    });
-}
+  const {
+    record,
+    moduleInfo: { primarykey, namefield },
+  } = params;
+  const username = record[namefield];
+  Modal.confirm({
+    title: `确定要重置用户『${username}』的密码吗?`,
+    icon: <ExclamationCircleOutlined />,
+    onOk() {
+      queryResetUserPassword({ userid: record[primarykey] }).then((response: any) => {
+        if (response.success)
+          message.success(`用户『${username}』的密码已重置为“123456”请通知其尽快修改!`);
+        else message.error(`用户『${username}』的密码已重置失败!`);
+      });
+    },
+  });
+};
 
 /**
  * 对公司人员创建一个默认的用户
- * @param params 
+ * @param params
  */
 const createPersonnelUser = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield, modulename: moduleName }, dispatch } = params;
-    queryCreatePersonnelUser({
-        personnelid: record[primarykey]
-    }).then((response: any) => {
-        const mess = `人员『${record[namefield]}』创建用户`;
-        if (response.success) {
-            Modal.info({
-                title: mess + '成功!',
-                width: 500,
-                content: <span
-                    dangerouslySetInnerHTML={{
-                        __html: '<br/>' + response.message +
-                            '<br/><br/>请尽快通知其修改密码，并在用户模块中给其设置权限！'
-                    }}></span>
-            })
-            refreshRecordByKey({ dispatch, moduleName, key: record[primarykey] });
-        } else
-            Modal.error({
-                title: mess + '失败!',
-                width: 500,
-                content: response.message,
-            })
-    })
-}
+  const {
+    record,
+    moduleInfo: { primarykey, namefield, modulename: moduleName },
+    dispatch,
+  } = params;
+  queryCreatePersonnelUser({
+    personnelid: record[primarykey],
+  }).then((response: any) => {
+    const mess = `人员『${record[namefield]}』创建用户`;
+    if (response.success) {
+      Modal.info({
+        title: `${mess}成功!`,
+        width: 500,
+        /* eslint-disable */
+        content: (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: `<br/>${response.message}<br/><br/>请尽快通知其修改密码，并在用户模块中给其设置权限！`,
+            }}
+          />
+        ),
+        /* eslint-enable */
+      });
+      refreshRecordByKey({ dispatch, moduleName, key: record[primarykey] });
+    } else
+      Modal.error({
+        title: `${mess}失败!`,
+        width: 500,
+        content: response.message,
+      });
+  });
+};
 /**
  * 根据选中的字段生成模块的excel模板，再加工一下就可以上传
- * @param params 
+ * @param params
  */
 const exportExcelTemplate = (params: ActionParamsModal) => {
-    const { moduleInfo, records } = params;
-    const { primarykey } = moduleInfo;
-    const fieldids = records?.map((record: any) => record[primarykey]);
-    download('/api/platform/dataobjectexport/exportexceltemplate.do', {
-        fieldids,
-    })
-}
+  const { moduleInfo, records } = params;
+  const { primarykey } = moduleInfo;
+  const fieldids = records?.map((record: any) => record[primarykey]);
+  download('/api/platform/dataobjectexport/exportexceltemplate.do', {
+    fieldids,
+  });
+};
 
 /**
  * 显示用户的操作权限
  * 设置在grid定义的Drawer的参数，可以控制显示的内容
- * @param params 
+ * @param params
  */
 const displayUserLimit = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield }, funcDefine } = params;
-    const props: UserDrawerProps = {
-        visible: true,
-        title: <><span className={funcDefine.iconcls}></span>{` 用户『${record[namefield]}』的操作权限`}</>,
-        width: '60%',
-        children: <DisplayUserLimits userid={record[primarykey]} timestramp={new Date().getTime()} />,
-        onClose: () => setGlobalDrawerProps((props: any) => ({ ...props, visible: false })),
-    }
-    setGlobalDrawerProps(props);
-}
+  const {
+    record,
+    moduleInfo: { primarykey, namefield },
+    funcDefine,
+  } = params;
+  const props: UserDrawerProps = {
+    visible: true,
+    title: (
+      <>
+        <span className={funcDefine.iconcls} />
+        {` 用户『${record[namefield]}』的操作权限`}
+      </>
+    ),
+    width: '60%',
+    children: <DisplayUserLimits userid={record[primarykey]} timestramp={new Date().getTime()} />,
+    onClose: () => setGlobalDrawerProps(() => ({ visible: false })),
+  };
+  setGlobalDrawerProps(props);
+};
 
 /**
  * 设置用户的操作权限，这个是角色的辅助，在对于某个人有特殊权限时使用，一般用角色，好控制。
- * @param params 
+ * @param params
  */
 const setUserLimit = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield }, funcDefine } = params;
-    const msg = `用户『${record[namefield]}』操作权限设置`;
-    const props: UserDrawerProps = {
-        visible: true,
-        title: <><span className={funcDefine.iconcls}></span> {msg}</>,
-        width: '500px',
-        children: <>
-            <div style={{ margin: '0px 12px 12px' }}>
-                注意：此操作权限设置仅作为用户角色设置的辅助，只有某些用户有特殊权限时才进行设置，
-                否则请使用用户角色控制权限。(此操作权限会和用户角色中的权限叠加)
-            </div>
-            <SetUserLimits userid={record[primarykey]} msg={msg} timestramp={new Date().getTime()} /></>,
-        onClose: () => setGlobalDrawerProps((props: any) => ({ visible: false })),
-    }
-    setGlobalDrawerProps(props);
-}
+  const {
+    record,
+    moduleInfo: { primarykey, namefield },
+    funcDefine,
+  } = params;
+  const msg = `用户『${record[namefield]}』操作权限设置`;
+  const props: UserDrawerProps = {
+    visible: true,
+    title: (
+      <>
+        <span className={funcDefine.iconcls} /> {msg}
+      </>
+    ),
+    width: '500px',
+    children: (
+      <>
+        <div style={{ margin: '0px 12px 12px' }}>
+          注意：此操作权限设置仅作为用户角色设置的辅助，只有某些用户有特殊权限时才进行设置，
+          否则请使用用户角色控制权限。(此操作权限会和用户角色中的权限叠加)
+        </div>
+        <SetUserLimits userid={record[primarykey]} msg={msg} timestramp={new Date().getTime()} />
+      </>
+    ),
+    onClose: () => setGlobalDrawerProps(() => ({ visible: false })),
+  };
+  setGlobalDrawerProps(props);
+};
 
 /**
  * 显示系统操作角色的操作权限
- * @param params 
+ * @param params
  */
 const displayRoleLimit = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield }, funcDefine } = params;
-    const msg = `系统操作角色『${record[namefield]}』的操作权限`;
-    const props: UserDrawerProps = {
-        visible: true,
-        title: <><span className={funcDefine.iconcls}></span> {msg}</>,
-        width: '500px',
-        children: <SetRoleLimits display={true}
-            roleid={record[primarykey]} msg={msg} timestramp={new Date().getTime()} />,
-        onClose: () => setGlobalDrawerProps((props: any) => ({ visible: false })),
-    }
-    setGlobalDrawerProps(props);
-}
+  const {
+    record,
+    moduleInfo: { primarykey, namefield },
+    funcDefine,
+  } = params;
+  const msg = `系统操作角色『${record[namefield]}』的操作权限`;
+  const props: UserDrawerProps = {
+    visible: true,
+    title: (
+      <>
+        <span className={funcDefine.iconcls} /> {msg}
+      </>
+    ),
+    width: '500px',
+    children: (
+      <SetRoleLimits
+        display
+        roleid={record[primarykey]}
+        msg={msg}
+        timestramp={new Date().getTime()}
+      />
+    ),
+    onClose: () => setGlobalDrawerProps(() => ({ visible: false })),
+  };
+  setGlobalDrawerProps(props);
+};
 /**
  * 修改系统操作角色的操作权限
- * @param params 
+ * @param params
  */
 const setRoleLimit = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield }, funcDefine } = params;
-    const msg = `系统操作角色『${record[namefield]}』操作权限设置`;
-    const props: UserDrawerProps = {
-        visible: true,
-        title: <><span className={funcDefine.iconcls}></span> {msg}</>,
-        width: '500px',
-        children: <SetRoleLimits display={false}
-            roleid={record[primarykey]} msg={msg} timestramp={new Date().getTime()} />,
-        onClose: () => setGlobalDrawerProps((props: any) => ({ visible: false })),
-    }
-    setGlobalDrawerProps(props);
-}
-
-
+  const {
+    record,
+    moduleInfo: { primarykey, namefield },
+    funcDefine,
+  } = params;
+  const msg = `系统操作角色『${record[namefield]}』操作权限设置`;
+  const props: UserDrawerProps = {
+    visible: true,
+    title: (
+      <>
+        <span className={funcDefine.iconcls} /> {msg}
+      </>
+    ),
+    width: '500px',
+    children: (
+      <SetRoleLimits
+        display={false}
+        roleid={record[primarykey]}
+        msg={msg}
+        timestramp={new Date().getTime()}
+      />
+    ),
+    onClose: () => setGlobalDrawerProps(() => ({ visible: false })),
+  };
+  setGlobalDrawerProps(props);
+};
 
 /**
  * 定义一个工作流
  * @param {} param
  */
 const designWorkFlow = (params: ActionParamsModal) => {
-    const { record, dispatch } = params;
-    activitiModeler({ record, dispatch });
-}
+  const { record, dispatch } = params;
+  activitiModeler({ record, dispatch });
+};
 
 /**
  * 发布一个工作流
  * @param {} param
  */
 const deployWorkFlow = (params: ActionParamsModal) => {
-    const { record, moduleInfo: { primarykey, namefield, modulename: moduleName }, dispatch } = params;
-    const workflowid = record[primarykey];
-    request('/api/platform/workflowdesign/deploy.do', {
-        params: {
-            workflowid: workflowid
-        }
-    }).then((result) => {
-        if (result.success) {
-            message.success(`『${record[namefield]}』审批流程发布成功`);
-            dispatch({
-                type: 'modules/refreshRecord',
-                payload: {
-                    moduleName,
-                    recordId: workflowid,
-                },
-            })
-        } else {
-            Modal.error({
-                width: 500,
-                title: '记录保存时发生错误',
-                content: result.msg,
-            });
-        }
-    })
-}
+  const {
+    record,
+    moduleInfo: { primarykey, namefield, modulename: moduleName },
+    dispatch,
+  } = params;
+  const workflowid = record[primarykey];
+  request('/api/platform/workflowdesign/deploy.do', {
+    params: {
+      workflowid,
+    },
+  }).then((result) => {
+    if (result.success) {
+      message.success(`『${record[namefield]}』审批流程发布成功`);
+      dispatch({
+        type: 'modules/refreshRecord',
+        payload: {
+          moduleName,
+          recordId: workflowid,
+        },
+      });
+    } else {
+      Modal.error({
+        width: 500,
+        title: '记录保存时发生错误',
+        content: result.msg,
+      });
+    }
+  });
+};
 
 interface ActionStore {
-    [actionName: string]: Function;
+  [actionName: string]: Function;
 }
 
 /**
  * 所有的系统附加操作的函数的定义区域
  */
-export const systemActions: ActionStore = apply({
-    'createPersonnelUser': createPersonnelUser,
-    'resetpassword': resetUserPassword,
-    'displayuserlimit': displayUserLimit,
-    'setuserlimit': setUserLimit,
-    'exportExcelTemplate': exportExcelTemplate,
-    'displayrolelimit': displayRoleLimit,
-    'setrolelimit': setRoleLimit,
-    'designworkflow': designWorkFlow,
-    'deployworkflow': deployWorkFlow,
-    'importtableandview': importTableAndView,
+export const systemActions: ActionStore = apply(
+  {
+    createPersonnelUser,
+    resetpassword: resetUserPassword,
+    displayuserlimit: displayUserLimit,
+    setuserlimit: setUserLimit,
+    exportExcelTemplate,
+    displayrolelimit: displayRoleLimit,
+    setrolelimit: setRoleLimit,
+    designworkflow: designWorkFlow,
+    deployworkflow: deployWorkFlow,
+    importtableandview: importTableAndView,
     // 实体对象和实体对象共用
-    'refreshfields': refreshFields,
+    refreshfields: refreshFields,
     // 数据源的操作
-    'testDataSource': testDataSource,
-    'breakDataSource': breakDataSource,
-    'importSchema': importSchema,
-    'importSchemaTable': dataSourceImportTableAndView,
-
-}, businessActions) as ActionStore;
+    testDataSource,
+    breakDataSource,
+    importSchema,
+    importSchemaTable: dataSourceImportTableAndView,
+  },
+  businessActions,
+) as ActionStore;
--- src/pages/module/additionalAction/systemActionService.ts
+++ src/pages/module/additionalAction/systemActionService.ts
@@ -1,83 +1,82 @@
-import request, { syncRequest } from "@/utils/request";
+import request from '@/utils/request';
 import { serialize } from 'object-to-formdata';
 /**
  * 根据传入的人员id,生成该人员的用户
- * @param params 
+ * @param params
  */
 export async function queryCreatePersonnelUser(params: any) {
-    return request(`/api/platform/systemframe/createpersonnaluser.do`, {
-        method: 'POST',
-        params,
-    })
+  return request(`/api/platform/systemframe/createpersonnaluser.do`, {
+    method: 'POST',
+    params,
+  });
 }
 
 /**
  * 重置用户密码
- * @param params 
+ * @param params
  */
 export async function queryResetUserPassword(params: any) {
-    return request(`/api/platform/systemframe/resetpassword.do`, {
-        method: 'POST',
-        params,
-    })
+  return request(`/api/platform/systemframe/resetpassword.do`, {
+    method: 'POST',
+    params,
+  });
 }
 
 /**
  * 读取用户的所有操作权限,是所有权限和角色的并集
- * @param params 
+ * @param params
  */
 export async function queryUserAllLimits({ userid }: { userid: string }) {
-    return request('/api/platform/userrole/getuseralllimit.do', {
-        params: {
-            userid
-        },
-    });
+  return request('/api/platform/userrole/getuseralllimit.do', {
+    params: {
+      userid,
+    },
+  });
 }
 
 /**
  * 读取用户的所有操作权限，按照树形结构，选中的都有checked的标记
- * @param param0 
+ * @param param0
  */
 export async function queryUserLimits({ userid }: { userid: string }) {
-    return request('/api/platform/userrole/getuserlimit.do', {
-        params: {
-            userid,
-            addall: true,
-        },
-    });
+  return request('/api/platform/userrole/getuserlimit.do', {
+    params: {
+      userid,
+      addall: true,
+    },
+  });
 }
 
 /**
  * 保存用户所有选中的操作权限
- * @param param0 
+ * @param param0
  */
 export async function saveUserLimits(params: any) {
-    return request('/api/platform/userrole/saveuserlimit.do', {
-        method: 'POST',
-        body: serialize(params),
-    });
+  return request('/api/platform/userrole/saveuserlimit.do', {
+    method: 'POST',
+    body: serialize(params),
+  });
 }
-
 
 /**
  * 读取系统操作角色的所有操作权限，按照树形结构，选中的都有checked的标记
- * @param param0 
+ * @param param0
  */
 export async function queryRoleLimits({ roleid }: { roleid: string }) {
-    return request('/api/platform/userrole/getrolelimit.do', {
-        params: {
-            roleid
-        },
-    });
+  return request('/api/platform/userrole/getrolelimit.do', {
+    params: {
+      roleid,
+    },
+  });
 }
 
 /**
  * 保存系统操作角色所有选中的操作权限
- * @param param0 
+ * @param param0
  */
 export async function saveRoleLimits(params: any) {
-    return request('/api/platform/userrole/saverolelimit.do', {
-        method: 'POST',
-        body: serialize(params),
-    });
+  return request('/api/platform/userrole/saverolelimit.do', {
+    method: 'POST',
+    body: serialize(params),
+  });
 }
