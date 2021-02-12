c8658fc72f5876cc3f067828653e20824cfa7ce7
jfok1972 <jfok1972@qq.com>
2021-02-12 14:15:03 +0800

    init

--- src/pages/module/dictionary/service.ts
+++ src/pages/module/dictionary/service.ts
@@ -1,36 +1,38 @@
-
 import request, { syncRequest } from '@/utils/request';
 import { serialize } from 'object-to-formdata';
 
+const DC = '_dc';
+
 export async function queryDicrionaryDefine(params: any) {
-    return request(`/api/dictionary/getdictionary.do`, {
-        method: 'POST',
-        body: serialize(params),
-    })
+  return request(`/api/dictionary/getdictionary.do`, {
+    method: 'POST',
+    body: serialize(params),
+  });
 }
 
 export async function queryDictionaryData(params: any) {
-    params._dc = new Date().getTime();
-    return request(`/api/dictionary/getDictionaryComboData.do`, {
-        params
-    })
+  return request(`/api/dictionary/getDictionaryComboData.do`, {
+    params: {
+      ...params,
+      [DC]: new Date().getTime(),
+    },
+  });
 }
 
-
 export function querySyncDicrionaryDefine(params: any): any {
-    return syncRequest(`/api/dictionary/getdictionary.do`, {
-        params,
-    })
+  return syncRequest(`/api/dictionary/getdictionary.do`, {
+    params,
+  });
 }
 
 export function querySyncDictionaryData(params: any): any {
-    return syncRequest(`/api/dictionary/getDictionaryComboData.do`, {
-        params
-    })
+  return syncRequest(`/api/dictionary/getDictionaryComboData.do`, {
+    params,
+  });
 }
 
 export function querySyncPropertys(params: any): any {
-    return syncRequest(`/api/dictionary/getPropertyComboData.do`, {
-        params,
-    })
+  return syncRequest(`/api/dictionary/getPropertyComboData.do`, {
+    params,
+  });
 }
