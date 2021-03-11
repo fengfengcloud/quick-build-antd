a217d3fe0ad301043a3d13a57d39ddf230c730c3
jfok1972 <jfok1972@qq.com>
2021-03-11 11:11:12 +0800

    init

--- src/pages/dashboard/charts/index.tsx
+++ src/pages/dashboard/charts/index.tsx
@@ -4,13 +4,19 @@ import { hasModuleInSysMenu } from '@/layouts/BasicLayout';
 import { PmCharts } from './pm';
 import Monitor from '../monitor';
 
-export default (): React.ReactNode => (
-  <Card
-    bordered={false}
-    bodyStyle={{ padding: '0px 0px 16px 0px', margin: '0px 16px 16px' }}
-    style={{ margin: '-8px' }}
-  >
-    {/* 工程管理系统的图表 */}
-    {hasModuleInSysMenu('PmAgreement') ? <PmCharts /> : <Monitor />}
-  </Card>
-);
+export default (): React.ReactNode => {
+  const hasBusinessCharts: boolean = hasModuleInSysMenu('PmAgreement');
+  if (hasBusinessCharts) {
+    return (
+      <Card
+        bordered={false}
+        bodyStyle={{ padding: '0px 0px 16px 0px', margin: '0px 16px 16px' }}
+        style={{ margin: '-8px' }}
+      >
+        {/* 工程管理系统的图表 */}
+        {hasModuleInSysMenu('PmAgreement') ? <PmCharts /> : null}
+      </Card>
+    );
+  }
+  return <Monitor />;
+};
