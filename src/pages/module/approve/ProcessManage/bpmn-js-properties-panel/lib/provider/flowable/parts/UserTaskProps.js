be92c761e3dd1751ec8c1640037fbfd7e99732c9
jfok1972 <jfok1972@qq.com>
2021-04-23 23:00:45 +0800

    init

--- src/pages/module/approve/ProcessManage/bpmn-js-properties-panel/lib/provider/flowable/parts/UserTaskProps.js
+++ src/pages/module/approve/ProcessManage/bpmn-js-properties-panel/lib/provider/flowable/parts/UserTaskProps.js
@@ -1,55 +1,97 @@
 'use strict';
 
 var is = require('bpmn-js/lib/util/ModelUtil').is,
-    entryFactory = require('bpmn-js-properties-panel/lib/factory/EntryFactory');
+  entryFactory = require('bpmn-js-properties-panel/lib/factory/EntryFactory');
 
+var syncRequest = require('../../../../../../../../../utils/request').syncRequest;
 
-module.exports = function(group, element, translate) {
+var users = [];
+var groups = [];
+
+function getUsers() {
+  if (users.length === 0) {
+    var usersResult = syncRequest("/api/platform/dataobject/fetchcombodata.do", {
+      params: {
+        moduleName: 'FUser'
+      }
+    });
+    users.push({ name: '', value: '' });
+    usersResult.forEach(function (rec) {
+      users.push({
+        name: rec.text,
+        value: rec.value
+      })
+    })
+  }
+  return users;
+}
+function getGroups() {
+  if (groups.length === 0) {
+    var usersResult = syncRequest("/api/platform/dataobject/fetchcombodata.do", {
+      params: {
+        moduleName: 'FRole'
+      }
+    });
+    groups.push({ name: '', value: '' });
+    usersResult.forEach(function (rec) {
+      groups.push({
+        name: rec.text,
+        value: rec.value
+      })
+    })
+  }
+  return groups;
+}
+
+module.exports = function (group, element, translate) {
   if (is(element, 'activiti:Assignable')) {
 
     // Assignee
-    group.entries.push(entryFactory.textField({
-      id : 'assignee',
-      label : translate('Assignee'),
-      modelProperty : 'assignee'
+    group.entries.push(entryFactory.selectBox({
+      id: 'activiti:assignee',
+      label: translate('Assignee'),
+      // emptyParameter: true,
+      selectOptions: getUsers(),
+      modelProperty: 'activiti:assignee'
     }));
 
     // Candidate Users
     group.entries.push(entryFactory.textField({
-      id : 'candidateUsers',
-      label : translate('Candidate Users'),
-      modelProperty : 'candidateUsers'
+      id: 'activiti:candidateUsers',
+      label: translate('Candidate Users'),
+      modelProperty: 'activiti:candidateUsers'
     }));
 
     // Candidate Groups
-    group.entries.push(entryFactory.textField({
-      id : 'candidateGroups',
-      label : translate('Candidate Groups'),
-      modelProperty : 'candidateGroups'
+    group.entries.push(entryFactory.selectBox({
+      id: 'activiti:candidateGroups',
+      label: translate('Candidate Groups'),
+      selectOptions: getGroups(),
+      modelProperty: 'activiti:candidateGroups'
     }));
 
     // Due Date
     group.entries.push(entryFactory.textField({
-      id : 'dueDate',
-      description : translate('The due date as an EL expression (e.g. ${someDate} or an ISO date (e.g. 2015-06-26T09:54:00)'),
-      label : translate('Due Date'),
-      modelProperty : 'dueDate'
+      id: 'dueDate',
+      description: translate('The due date as an EL expression (e.g. ${someDate} or an ISO date (e.g. 2015-06-26T09:54:00)'),
+      label: translate('Due Date'),
+      modelProperty: 'dueDate'
     }));
 
     // FollowUp Date
     group.entries.push(entryFactory.textField({
-      id : 'followUpDate',
-      description : translate('The follow up date as an EL expression (e.g. ${someDate} or an ' +
-                    'ISO date (e.g. 2015-06-26T09:54:00)'),
-      label : translate('Follow Up Date'),
-      modelProperty : 'followUpDate'
+      id: 'followUpDate',
+      description: translate('The follow up date as an EL expression (e.g. ${someDate} or an ' +
+        'ISO date (e.g. 2015-06-26T09:54:00)'),
+      label: translate('Follow Up Date'),
+      modelProperty: 'followUpDate'
     }));
 
     // priority
     group.entries.push(entryFactory.textField({
-      id : 'priority',
-      label : translate('Priority'),
-      modelProperty : 'priority'
+      id: 'priority',
+      label: translate('Priority'),
+      modelProperty: 'priority'
     }));
   }
 };
